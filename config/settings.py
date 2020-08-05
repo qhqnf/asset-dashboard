547fb135868a488f19d37f3f542914edc4ae4761
root <didguscjf95@naver.com>
2020-08-05 20:55:23 +0900

    update .gitignore

--- config/settings.py
+++ config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "w8)a@rvaisqiglyuy_)s_!&vnn2k+od!la5ht9t-g-1&ak^m%t"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG"))
 
 ALLOWED_HOSTS = []
 
