befd8689655b3b8dd817cc97fcdfb280af8eab74
root <didguscjf95@naver.com>
2020-08-06 01:40:53 +0900

    fix command

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -4,7 +4,10 @@ from users.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        admin = User.objects.get_or_none(username="ebadmin")
+        try:
+            admin = User.objects.get(username="ebadmin")
+        except User.objects.DoesNotExist:
+            admin = None
         if admin is None:
             User.objects.create_superuser("ebadmin", "didguscjf95@naver.com", "123")
             self.stdout.write(self.style.SUCCESS("Superuser created!"))
