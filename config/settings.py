a9b7a90a8e9d77c87114988d8546f973ae05033b
root <didguscjf95@naver.com>
2020-08-05 23:00:39 +0900

    bug fixing

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "w8)a@rvaisqiglyuy_)s_!&vnn2k+od!la5ht9t-g-1&ak^m%t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
