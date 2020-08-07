9a4fde15439888fb851dcb7ddc43f27db9e7e0d6
root <didguscjf95@naver.com>
2020-08-07 19:18:26 +0900

    bugfixing

--- stocks/models.py
+++ stocks/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Stock(models.Model):
 
     code = models.CharField(max_length=10, primary_key=True)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=40)
 
     def __str__(self):
         return self.name
